4b9d474fb7c29e6c274c3ca9b6f6ff9fe9319f02
Prithaj Nath <prithajnath@gmail.com>
2016-11-09 18:01:04 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+var myCircle = new Path.Circle(new Point(100, 70), 50);
+myCircle({});
+
+myCircle.strokeColor = 'black';
+
+
+var myCircle2 = myCircle.clone()
+myCircle.strokecolor = 'red';
+myCircle2.position.x += 200;
+
+function onKeyDown(event){
+
+	if(event.key == 'w'){
+		myCircle2.position.y -= 10;
+	}
+
+	if(event.key == 'a'){
+		myCircle2.position.x -= 10;
+	}
+
+	if(event.key == 'd'){
+		myCircle2.position.x += 10;
+	}
+	
+	if(event.key == 's'){
+		myCircle2.position.y += 10;
+	}
+
+
+
+
+}
