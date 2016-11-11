97cd7376f25a5772d2ac926b83ca7da32e8df012
Prithaj <prithajnath@gmail.com>
2016-11-11 14:01:33 +0000

    Add left dribble

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 var myCircle = new Path.Circle(new Point(150, 70), 20);
 myCircle['fillColor'] = 'red';
-
 myCircle.strokeColor = 'black';
 
-function straight_line(ball,circle){
-	y = ((circle[0] + 20) - ball[0])*(circle[1] - ball[1])/(circle[0] - ball[0]);
+function straight_line(ball,circle,sign){
+	if(sign == '+'){
+		y = ((circle[0] + 20) - ball[0])*(circle[1] - ball[1])/(circle[0] - ball[0]);
+	}else{
+		y = ((circle[0] - 20) - ball[0])*(circle[1] - ball[1])/(circle[0] - ball[0]);	
+	}
+
 	return y;
 };
 
@@ -35,16 +39,17 @@ function onKeyDown(event){
 	}
 
 	if((myCircle.getIntersections(main_ball)).length > 0){
-		arr = myCircle.getIntersections(main_ball);
-		//console.log([main_ball.segments[1].point.x,main_ball.segments[0].point.y]);
-		//console.log([main_ball.position.x,main_ball.position.y])
-		//console.log([myCircle.segments[1].point.x,myCircle.segments[0].point.y]);
+		//arr = myCircle.getIntersections(main_ball);
 		x_ball = [main_ball.position.x,main_ball.position.y];
 		x_circle = [myCircle.position.x, myCircle.position.y];
-		main_ball.position.x += 20;
-		main_ball.position.y += straight_line(x_ball, x_circle);
+		if(x_circle[0]>x_ball[0]){
+			main_ball.position.x -= 20;
+			main_ball.position.y -= straight_line(x_ball, x_circle,'-');
+		}else{
+			main_ball.position.x += 20;
+			main_ball.position.y += straight_line(x_ball, x_circle),'+';
+		}
 		
-
 	}
 
 }
@@ -54,6 +59,5 @@ var main_ball = new Path.Circle({
 	center: [250,100],
 	radius: 10
 });
+
 main_ball.strokeColor = 'black';
-//console.log("Here is the center");
-//console.log(main_ball);
